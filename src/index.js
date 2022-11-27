85652b2f2b20d9ae1c1e58fd2e08a2b8c6e3baf6
damanatidis00 <damanatidis00@gmail.com>
2022-11-27 17:39:46 +0200

    Added forecast

--- src/index.js
+++ src/index.js
@@ -43,11 +43,65 @@ if (currentMinutes < 10) {
   currentTime.innerHTML = `${currentHour}:${currentMinutes}`;
 }
 
+function formatDate(timestamp) {
+  now = new Date(timestamp);
+  let day = days[now.getDay()];
+  let month = months[now.getMonth()];
+  let date = now.getDate();
+  let year = now.getFullYear();
+  let cityHour = now.getHours();
+  let cityMinutes = now.getMinutes();
+
+  let currentDate = document.querySelector("#date");
+  currentDate.innerHTML = `${day}, ${month} ${date}, ${year}`;
+
+  let currentTime = document.querySelector("#time");
+  if (cityMinutes < 10) {
+    currentTime.innerHTML = `${cityHour}:0${cityMinutes}`;
+  } else {
+    currentTime.innerHTML = `${cityHour}:${cityMinutes}`;
+  }
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function showForcast(response) {
+  console.log(response);
+  let forecast = response.data.daily;
+  let weatherCard = document.querySelector("#weather-card");
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        ` <div class="col">
+    <div class="weathercard">
+    <img src="${forecastDay.condition.icon_url}">
+      <p>${formatDay(forecastDay.time)}</p>
+     
+      <p class="smalltext"> ${Math.round(
+        forecastDay.temperature.maximum
+      )}℃ / ${Math.round(forecastDay.temperature.minimum)}℃</p>
+    </div>
+  </div>`;
+    }
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  weatherCard.innerHTML = forecastHTML;
+}
+
 /*Search City Weather Code / API Code */
 
 function showCurrentWeather(response) {
   console.log(response);
-
+  let timestamp = response.data.time * 1000;
+  formatDate(timestamp);
   currentTemperature = response.data.temperature.current;
   temperatureElement.innerHTML = Math.round(currentTemperature);
   let windElement = document.querySelector("#wind");
@@ -75,7 +129,9 @@ function searchCity(event) {
 
   let key = "t3b440bad7o6b9c0b3f432b3e3d4d1d5";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${searchInput.value}&key=${key}`;
+  let forcastUrl = `https://api.shecodes.io/weather/v1/forecast?query=${searchInput.value}&key=${key}`;
   axios.get(apiUrl).then(showCurrentWeather);
+  axios.get(forcastUrl).then(showForcast);
 }
 
 let searchButton = document.querySelector("#searchButton");
@@ -86,6 +142,8 @@ function getCoord(position) {
   let lon = position.coords.longitude;
   let key = "t3b440bad7o6b9c0b3f432b3e3d4d1d5";
   let apiURL = `https://api.shecodes.io/weather/v1/current?lon=${lon}&lat=${lat}&key=${key}`;
+  let forcastUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${lon}&lat=${lat}&key=${key}`;
+  axios.get(forcastUrl).then(showForcast);
   axios.get(apiURL).then(showCurrentWeather);
 }
 
@@ -96,30 +154,8 @@ function openNavigator(event) {
 let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", openNavigator);
 
-function changeToCelcius(event) {
-  event.preventDefault();
-  celciusButton.classList.add("active");
-  FahrenheitButton.classList.remove("active");
-  temperatureElement.innerHTML = Math.round(currentTemperature);
-}
-
-function changeToFahrenheit(event) {
-  event.preventDefault();
-  celciusButton.classList.remove("active");
-  FahrenheitButton.classList.add("active");
-  temperatureElement.innerHTML = Math.round(currentTemperature * (9 / 5) + 32);
-}
-
-let celciusButton = document.querySelector("#celcius-indicator");
-celciusButton.addEventListener("click", changeToCelcius);
-
-let FahrenheitButton = document.querySelector("#fahrenheit-indicator");
-FahrenheitButton.addEventListener("click", changeToFahrenheit);
-
 function search(city) {
   let key = "t3b440bad7o6b9c0b3f432b3e3d4d1d5";
   let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${key}`;
   axios.get(apiURL).then(showCurrentWeather);
 }
-
-search("Athens");
